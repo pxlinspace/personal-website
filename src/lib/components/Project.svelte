a64266bf92b478a2cad3f5e12f0e3642864c81fc
pxlinspace <108901892+pxlinspace@users.noreply.github.com>
2026-08-14 00:52:37 -0700

    added project thumbnails & working on styling

--- src/lib/components/Project.svelte
+++ src/lib/components/Project.svelte
@@ -2,21 +2,39 @@
 	let { href, imgSrc, title, titleColor = 'var(--foreground-color)', text } = $props();
 </script>
 
-<div>
-	<img class="bordered" src={imgSrc} alt="thumbnail" />
-	<!-- <span>
-		<span style="color: {titleColor}">{title}</span>:
-		<span class="text">{text}</span>
-	</span> -->
+<div class="project">
+	<div class="image-frame">
+		<img src={imgSrc} alt="thumbnail" />
+	</div>
+	<!-- <span style="color: {titleColor}">{title}</span> -
+	<span class="text">{text}</span> -->
 </div>
 
 <style>
-	div {
-		padding: 16px;
+	.project {
+		width: 100%;
+		aspect-ratio: 16 / 9;
+	}
+	.image-frame {
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		transition: height 0.2s ease-out;
 	}
 
-	img {
+	.image-frame img {
 		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		display: block;
+		transform: scale(1);
+		transition: transform 0.2s ease-in-out;
+	}
+
+	.project:hover {
+		.image-frame img {
+			transform: scale(1.1);
+		}
 	}
 
 	.text {
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Project from '$lib/components/Project.svelte';
 
-	import dreamskater from '$lib/assets/images/project_thumbnails/dreamskater.png';
+	import { projectThumbnailMap as thumbnails } from '$lib/imageMaps';
 </script>
 
 <svelte:head>
@@ -16,25 +16,31 @@
 
 <div class="project-container">
 	<Project
-		imgSrc={dreamskater}
+		imgSrc={thumbnails.dreamskater}
 		title="DREAMSKATER"
 		titleColor="#6c0d46"
 		text="A precision puzzle game with only one button - made for GMTK Game Jam 2025"
 	/>
 	<Project
-		imgSrc={dreamskater}
+		imgSrc={thumbnails.pixipaint}
+		title="Pixipaint"
+		titleColor="#6c0d46"
+		text="A Picross-inspired puzzle game and also the first project I've made in next.js"
+	/>
+	<Project
+		imgSrc={thumbnails.spadooshka}
 		title="Spadooshka"
 		titleColor="#6c0d46"
 		text="1v1 first-person shooter focused on platforming - made for Hack Club's Juice game jam"
 	/>
 	<Project
-		imgSrc={dreamskater}
+		imgSrc={thumbnails.chatgpme}
 		title="ChatGPMe"
 		titleColor="#6c0d46"
 		text="A game where you are the LLM answering peoples' prompts - Hack the North 2024 finalist"
 	/>
 	<Project
-		imgSrc={dreamskater}
+		imgSrc={thumbnails.spacecatadventure}
 		title="Spacecat Adventure"
 		titleColor="#6c0d46"
 		text="A short platformer and the first game I've ever released online"
@@ -43,7 +49,7 @@
 
 <style>
 	.white-overlay {
-		background-color: rgb(255, 255, 255, 0.75);
+		background-color: rgb(255, 255, 255, 0.8);
 		position: fixed;
 		top: 0;
 		left: 0;
@@ -53,7 +59,13 @@
 	}
 	.project-container {
 		display: grid;
-		grid-template-columns: auto auto;
-		padding: 32px;
+		grid-template-columns: 1fr 1fr;
+		gap: 24px;
+	}
+
+	@media screen and (max-width: 700px) {
+		.project-container {
+			grid-template-columns: 1fr;
+		}
 	}
 </style>
